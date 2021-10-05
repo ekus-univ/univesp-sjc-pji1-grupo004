780e75d22db7bd9bc796839fb398635108c2da5e
Ronai Melo <amzronai@gmail.com>
2021-10-05 18:01:22 -0300

    Modela banco de dados

--- divulga_sjc/webpage/models.py
+++ divulga_sjc/webpage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length = 200)
+    
+class Negocio(models.Model):
+    categoria = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=800)
+    instagram = models.CharField(max_length=200)
+    facebook = models.CharField(max_length=200)
+    whatsapp = models.CharField(max_length=200)
+    
