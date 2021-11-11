10a0e036a7e4d5ec87f68734f9d7bc824a607d0f
Ronai Melo <amzronai@gmail.com>
2021-11-11 11:16:01 -0300

    Atualiza parte de front-end do projeto

--- feirasjc/feirapage/static/feirapage/script.js
+++ feirasjc/feirapage/static/feirapage/script.js
@@ -11,3 +11,29 @@ window.addEventListener('scroll', function() {
 
     }
 })
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    //opera pelo mouse e teclado
+    mousewheel: true,
+    keyboard: true
+});
+
+// Scroll Reveal 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1000,
+    reset: true
+});
+
+sr.reveal(
+`#home img, #home .text,
+#about img, #about .text,
+#services img, #services .text,
+#testimonials img, #testimonials .text,
+#contact img, #contact .text`
+, { interval: 100});
