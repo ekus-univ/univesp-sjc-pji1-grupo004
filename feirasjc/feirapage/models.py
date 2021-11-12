65f23a8ae991062de476ab1dc6788bd54d25ef99
ekus-univ <90095994+ekus-univ@users.noreply.github.com>
2021-11-12 18:27:16 -0300

    Adicionado classe Post

--- feirasjc/feirapage/models.py
+++ feirasjc/feirapage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField('Título', max_length=200)
+    author = models.CharField('Autor', max_length=100)
+    text = models.TextField('Depoimento')
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.title
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
