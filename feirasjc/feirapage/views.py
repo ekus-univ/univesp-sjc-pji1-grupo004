f64048daeb1b4345ec208db8cd51554a5eec7174
ekus-univ <90095994+ekus-univ@users.noreply.github.com>
2021-11-12 18:30:20 -0300

    Modificado função index()

--- feirasjc/feirapage/views.py
+++ feirasjc/feirapage/views.py
@@ -1,6 +1,22 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+from .forms import PostForm
+from django.utils import timezone
+
+
 def index(request):
-    return render(request,"base.html")
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+             post = form.save(commit=False)
+             post.pub_date = timezone.now()
+             form.save()
+             return HttpResponseRedirect('/#testimonials')
+    else:
+        form = PostForm()
+    return render(request, 'base.html', {'form': form, 'latest_post_list': latest_post_list,})
+    
